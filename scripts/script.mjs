de6483787d7fd79622cba3bc5d924bf561e88726
prateeksingh132 <kumarpks132@gmail.com>
2026-01-17 22:09:56 -0700

    Planning the project. creating a roadmap, added goals, and objectives. will add more as i explore more

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -9,11 +9,23 @@
 
 
 /////////GOAL
+// i have to use all the concepts in Async Javascript in this SBA.
+// my goal is to create a home page (), "Shop" page for GadgetShack that pulls real products from an API. i am gonna continue preparing my full stack ecommerce website (capstone project).
+// i did a google search for some fake store apis, found https://dummyjson.com/.
+// i want my pages to be dynamic. i wanna to show "Loading..." when data is coming, and show error if it fails.
+// since, i am using this sba for my capstone prep, i have already created the registration page in my SBA 316, in this sba, i am gonna create a homepage and a catalog page calles Shop which will pull the data from the api.
+// i also want to save items to cart using localStorage. will have to figure it out how to do it later.
+// i am gonna use fetch.
 
 
 
-///////// Things to keep in mind:
-
+///////// Things to keep in mind (requirement list):
+// create an external json/api request. the api url https://dummyjson.com/products has data in JSON format.
+// create a function to add the data to the DOM.
+// use async/await for data fetching.
+// implement error handling (try/catch).
+// use at least 3 module files (scripts). figure out how to divide the script work in 3 modules.
+// BOM: use window object (alert or something).
 
 
 
@@ -23,7 +35,7 @@
 
 
 //////// WHAT I KNOW:
-
+// the api url is 'https://dummyjson.com/products'
 
 
 
