1f5a2df482ebcd807b0b4b0ceb8203437f06baa4
prateeksingh132 <kumarpks132@gmail.com>
2026-01-17 17:16:26 -0700

    Starting main work. Added objectives and writing my thinking process

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -1,4 +1,41 @@
 
+///// Objectives from assignment doc
+// Use asynchronous JavaScript tools to build a responsive web application.
+// Demonstrate understanding of the JavaScript event loop.
+// Generate asynchronous code using Promises and async/await syntax.
+// Use fetch and/or Axios to interact with an external web API.
+// Organize files using modules and imports.
+
+
+
+/////////GOAL
+
+
+
+///////// Things to keep in mind:
+
+
+
+
+//// Remember (from dylan/lecture):
+
+
+
+
+//////// WHAT I KNOW:
+
+
+
+
+
+////////LOGIC - step by step
+
+
+
+
+
+
+
 //////////////////////////////////// SBA 308A
 
 console.log("\n");
