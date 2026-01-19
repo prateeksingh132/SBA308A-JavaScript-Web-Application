52539919cb8fe60788c2f50dfedc39ebd2e643a4
prateeksingh132 <kumarpks132@gmail.com>
2026-01-18 18:04:54 -0700

    Done with api module, created a single function to fetch json data, exported it, used async/await and tested, it works till here. gonna work on ui.mjs now and then main script.

--- scripts/api.mjs
+++ scripts/api.mjs
@@ -19,4 +19,62 @@ const baseUrl = 'https://dummyjson.com/products';
 // it takes a category string, fetches the data, and RETURNS it.
 export async function fetchTheData(categoryName) {
 
+    // step 2: URL construction
+    // logic: if category is 'all', i use base url. base url has all the json data, it includes all tech products along with other products (non-tech).
+    // if its something else, i m gonna modify the url.
+    let finalUrl = baseUrl;
+    if (categoryName && categoryName !== 'all') {
+        finalUrl = `${baseUrl}/category/${categoryName}`;
+    }
+
+    // step 3: main logic here, put your code isnide the try/catch block
+    try {
+
+        ////////////TESTING
+        //console.log('TESTING: finalUrl: ', finalUrl);
+        ////////////
+
+        console.log(`Fetching data from: ${finalUrl}`);
+
+        // have to use await here as well as in data, remember fetch can take time to have to await it. code pauses here until data comes back.
+        const response = await fetch(finalUrl);
+
+
+        ////////////TESTING
+        //console.log('TESTING: response.ok: ', response.ok);
+        ////////////
+
+        // validation is key here. if response is not 200 ok, i m gonna throw error manually.
+        if (!response.ok) {
+            throw new Error(`HTTP Error! status: ${response.status}`);
+        }
+
+        // convert response to json (use await here too)
+        const data = await response.json();
+
+        ////////////TESTING
+        //console.log('TESTING: data: ', data);
+        ////////////
+
+        // LOGIC CHECK: 
+        // the api returns an object, and the actual array is inside a property called 'products'.
+        // so i need to access data.products, not just data.
+
+
+        ////////////TESTING
+        //console.log('TESTING: data.products: ', data.products);
+        ////////////
+
+        // logic: return the products array so main.js can handle it
+        return data.products;
+
+    } catch (err) {
+        console.error("Error fetching data:", err.message);
+    }
+
 }
+
+
+////////////TESTING
+//fetchTheData("all");
+////////////
